d5555770c3707b9d098b05894650a2686b78a961
Saran Raaj <84842445+Saranraaj@users.noreply.github.com>
2022-03-22 04:43:35 +0530

    Add files via upload

---
+++ part01/App_1.2.js
@@ -0,0 +1,66 @@
+import "./styles.css";
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  const total = exercises1 + exercises2 + exercises3;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        exercises={{
+          part1: part1,
+          exercises1: exercises1,
+          part2: part2,
+          exercises2: exercises2,
+          part3: part3,
+          exercises3: exercises3
+        }}
+      />
+
+      <Total total={total} />
+    </div>
+  );
+};
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+const Content = (props) => {
+  return (
+    <div>
+      <Part
+        part={props.exercises.part1}
+        exercise={props.exercises.exercises1}
+      />
+      <Part
+        part={props.exercises.part2}
+        exercise={props.exercises.exercises2}
+      />
+      <Part
+        part={props.exercises.part3}
+        exercise={props.exercises.exercises3}
+      />
+    </div>
+  );
+};
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  );
+};
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+export default App;
